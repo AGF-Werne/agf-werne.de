05f3a7f5efb3f851523255a44fa89967d0152037
Danilo Klose <klose.danilo@gmail.com>
2016-04-29 15:32:07 +0200

    added app folder (again)

---
+++ app/app.component.ts
@@ -0,0 +1,48 @@
+/**
+ * @file app/app.component.ts
+ */
+import { Component } from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
+import { Home } from './home/home.component.ts';
+import { Impressum } from './impressum/impressum.component.ts';
+
+@Component({
+    selector: 'agf-app',
+    directives: [
+        ROUTER_DIRECTIVES
+    ],
+    template: `
+        <div class="app">
+            <nav>
+                <ul>
+                    <li>
+                        <a [routerLink]=" ['Home'] ">Home</a>
+                    </li>
+                    <li>
+                        <a [routerLink]=" ['Impressum'] ">Impressum</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <main>
+                <router-outlet></router-outlet>
+            </main>
+        </div>
+    `
+})
+
+@RouteConfig([
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        useAsDefault: true
+    },
+    {
+        path: '/impressum',
+        name: 'Impressum',
+        component: Impressum
+    }
+])
+
+export class AppComponent { }
---
+++ app/impressum/impressum.component.ts
@@ -0,0 +1,17 @@
+/**
+ * @file app/impressum/impressum.component.ts
+ */
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'impressum',
+    template: `
+        <h1>Impressum view!</h1>
+    `
+})
+
+export class Impressum {
+    constructor() {
+
+    }
+}
