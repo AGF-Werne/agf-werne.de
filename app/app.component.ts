229ca45b4bb3747444da026aedae3f97d084bb03
Danilo Klose <klose.danilo@gmail.com>
2016-04-29 11:18:00 +0200

    added app folder and changed name of app component

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+// app.component.ts
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'agf-app',
+    template: `
+        <div class="app">
+            Hello world!
+            <main>
+                <!-- stuffy -->
+            </main>
+        </div>
+    `
+})
+
+export class AppComponent {}
