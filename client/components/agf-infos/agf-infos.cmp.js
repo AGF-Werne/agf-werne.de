c168e2785498b1c90143fd8ab9d8695d512a8f23
Danilo Klose <klose.danilo@gmail.com>
2016-09-11 14:55:08 +0200

    updated infos section

--- client/components/agf-infos/agf-infos.cmp.js
+++ client/components/agf-infos/agf-infos.cmp.js
@@ -18,9 +18,10 @@
      * @namespace agfInfos
      * @memberof Components
      * @requires ___env
+     * @requires $location
      * @returns {Boolean}
      */
-    function controller (__env) {
+    function controller (__env, $location) {
         var vm = this;
 
         return init();
@@ -30,6 +31,13 @@
             var nav = __env[mode].navigation.main;
 
             vm.nav = nav;
+
+            if ($location.search().ggua) {
+                vm.content = 'components/agf-infos/agf-infos-ggua.html';
+            }
+            else {
+                vm.content = 'components/agf-infos/agf-infos-index.html';
+            }
 
             return true;
         }
